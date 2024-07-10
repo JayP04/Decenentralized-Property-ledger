5f64763264bc15340959612c4ed64aa76295e3ea
Jay Patel <jayrpatel2004>
2024-07-10 09:48:31 -0600

    Initial development environment setup

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Setting up the work environment for the project.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React organization
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
